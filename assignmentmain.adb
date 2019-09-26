19c65f4095e5758afc6ccb2af4d25eaaed963ad4
Vic Lee <viclee@nyu.edu>
2019-09-25 20:53:27 -0400

    Add ability read from pipe

--- assignmentmain.adb
+++ assignmentmain.adb
@@ -1,23 +1,13 @@
-with Text_Io;
-use Text_Io;
+with Ada.Command_Line, Text_Io;
+use  Ada.Command_Line, Text_Io;
+
 
 procedure AssignmentMain is
-    Input_File: File_Type;
+	Line : String(1..10);
+   	Length : Natural;
 begin
-    Open(File => Input_File, 
-         Mode => In_File, 
-         Name => "input.txt");
-    loop
-        declare
-            Line: String := Get_Line(Input_File);
-        begin
-            Put_Line(Line);
-        end;
-    end loop;
-    Close(Input_File);
-exception
-    when End_Error =>
-        if Is_Open(Input_File) then
-            Close(Input_File);
-        end if;
+	while not Text_Io.End_Of_File loop
+		Get_Line(Item => Line, Last => Length);
+		Put(Line);
+	end loop;
 end AssignmentMain;
