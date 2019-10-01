e64a1a63896fa153904f9ba792e7c0fdbc53f3b0
Vic Lee <viclee@nyu.edu>
2019-09-30 23:33:07 -0400

    Add logic to read integer by integer

--- assignmentmain.adb
+++ assignmentmain.adb
@@ -5,9 +5,16 @@ use  Ada.Command_Line, Text_Io;
 procedure AssignmentMain is
 	Line : String(1..10);
    	Length : Natural;
-begin
-	while not Text_Io.End_Of_File loop
-		Get_Line(Item => Line, Last => Length);
-		Put(Line);
-	end loop;
+
+   	task Reader1;
+   	task body Reader1 is
+   		counter : INTEGER range 1 .. MatrixMult.SIZE**2 := 1;
+   		Next_Int : INTEGER;
+   	begin
+		while not Text_Io.End_Of_File loop
+			Get(Next_Int);
+			Put(Next_Int);
+			New_Line;
+			counter := counter + 1;
+		end loop;
 end AssignmentMain;
